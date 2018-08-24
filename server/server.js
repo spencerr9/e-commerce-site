f3af535e957858e508e5c8aa6a89064859735482
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-08-24 12:21:55 -0600

    server is online

--- server/server.js
+++ server/server.js
@@ -1,5 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 const massive = require('massive');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express()
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+
+app.use(bodyParser.json())
+app.use(express.static( `${__dirname}/../build`))
+
+
+massive(CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance)
+}).catch(err => console.log(err))
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Don't touch my stuff. Port: ${SERVER_PORT}`)
+})
